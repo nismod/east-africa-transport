20bcf5c9d43d70ea649c2c802272be3775a9dd3d
Raghav Pant <raghavp@gmail.com>
2022-07-15 12:10:17 +0100

    modified rail network creation

--- scripts/preprocess/rail/africa_rail.py
+++ scripts/preprocess/rail/africa_rail.py
@@ -85,8 +85,6 @@ def main(config):
                             "network.geojson")))
     edges = convert_json_geopandas(edges)
     
-    print (nodes)
-    print (edges)
     # Create network topology
     network = create_network_from_nodes_and_edges(
         nodes,
@@ -103,26 +101,13 @@ def main(config):
         "rail",
         "rail.gpkg")
     
-    # network.edges.to_file(out_fname, layer='edges', driver='GPKG')
-    # network.nodes.to_file(out_fname, layer='nodes', driver='GPKG')
 
-    # print("Done exporting, ready to add attributes")
+    print("Done exporting, ready to add attributes")
 
 
-    # """Assign country info"""
-    
-    # out_fname = os.path.join(data_path,
-    #     "networks",
-    #     "rail",
-    #     "africa",
-    #     "africa-rails.gpkg")
-
-    # # Find the countries of the nodes and assign them to the node ID's, accordingly modify the edge ID's as well
-    # nodes = gpd.read_file(out_fname,layer='nodes')
-    # edges = gpd.read_file(out_fname,layer='edges')
-
+    """Assign country info"""
     edges = network.edges
-    nodes= network.nodes
+    nodes = network.nodes
 
     global_country_info = gpd.read_file(os.path.join(data_path,
         "Admin_boundaries",
@@ -131,11 +116,6 @@ def main(config):
     global_country_info = global_country_info.to_crs(epsg=3857)
     global_country_info = global_country_info.explode(ignore_index=True)
     global_country_info = global_country_info.sort_values(by="CONTINENT",ascending=True)
-
-    # nodes["node_id"] = nodes.progress_apply(lambda x:"_".join(x["node_id"].split("_")[1:]),axis=1)
-    # edges["edge_id"] = edges.progress_apply(lambda x:"_".join(x["edge_id"].split("_")[1:]),axis=1)
-    # edges["from_node"] = edges.progress_apply(lambda x:"_".join(x["from_node"].split("_")[1:]),axis=1)
-    # edges["to_node"] = edges.progress_apply(lambda x:"_".join(x["to_node"].split("_")[1:]),axis=1)
     
     # Set the crs
     edges = edges.to_crs(epsg=3857)
@@ -144,28 +124,16 @@ def main(config):
     nodes, edges = match_nodes_edges_to_countries(nodes,edges,global_country_info)
     print ("Done adding country info attributes")
 
-    # # Save as modified gpkg
-    # out_fname = os.path.join(data_path,
-    #                      "networks",
-    #                      "rail",
-    #                      "africa",
-    #                      "africa-rails-modified.gpkg")
-    # edges.to_file(out_fname, layer='edges', driver='GPKG')
-    # nodes.to_file(out_fname, layer='nodes', driver='GPKG')
+    """Assign rail attributes"""
 
-    # """Assign rail attributes"""
-
-    # nodes = gpd.read_file(out_fname,layer='nodes')
-    # edges = gpd.read_file(out_fname,layer='edges')
-
-    # # Set the crs
-    # edges = edges.to_crs(epsg=4326)
-    # nodes = nodes.to_crs(epsg=4326)
+    # Set the crs
+    edges = edges.to_crs(epsg=4326)
+    nodes = nodes.to_crs(epsg=4326)
 
     # Calculate and add length of line segments 
     geod = Geod(ellps="WGS84")
     edges['length_m'] = edges.progress_apply(lambda x:float(geod.geometry_length(x.geometry)),axis=1)
-
+    
     # Add speeds
     edges["speed_freight"] = edges.progress_apply(lambda x:round(0.001*x["length"]/(x["time_freight"]/60.0),2) if x["time_freight"] > 0 else 30.0,axis=1)
     speed_uncertainty = 0.1
