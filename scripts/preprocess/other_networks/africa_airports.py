c693e3390f51e06c44dcb0367082cda6b6ac7e1c
Raghav Pant <raghavp@gmail.com>
2022-07-15 14:22:07 +0100

    modified airports creation

--- scripts/preprocess/other_networks/africa_airports.py
+++ scripts/preprocess/other_networks/africa_airports.py
@@ -3,16 +3,10 @@
 """Process road data from OSM extracts and create road network topology 
 """
 import os
-from glob import glob
 import json
-import fiona
 import geopandas as gpd
 import pandas as pd
-import igraph as ig
-from geopy import distance
-import shapely.geometry
-from shapely.geometry import Point,LineString
-from boltons.iterutils import pairwise
+from shapely.geometry import Point
 from tqdm import tqdm
 tqdm.pandas()
 from utils import *
@@ -21,14 +15,14 @@ def main(config):
     incoming_data_path = config['paths']['incoming_data']
     data_path = config['paths']['data']
     
-    nodes = pd.read_csv(os.path.join(data_path,"airports","airport_nodes.csv"))
+    nodes = pd.read_csv(os.path.join(incoming_data_path,"airports","airport_nodes.csv"))
     nodes["node_id"] = nodes.index.values.tolist()
     nodes["node_id"] = nodes.progress_apply(lambda x:f"{x.iso_code}_airport_{x.node_id}",axis=1)
     nodes["geometry"] = nodes.progress_apply(lambda x: Point(x.lon,x.lat),axis=1)
     nodes = gpd.GeoDataFrame(nodes,geometry="geometry",crs="EPSG:4326")
     print (nodes)
 
-    nodes.to_file(os.path.join(data_path,"africa/networks","airports_modified.gpkg"),layer="nodes",driver="GPKG")
+    nodes.to_file(os.path.join(data_path,"networks/airports","air.gpkg"),layer="nodes",driver="GPKG")
 
 if __name__ == '__main__':
     CONFIG = load_config()
